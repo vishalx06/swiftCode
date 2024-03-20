66ff5e5d7bad7f39e654eb4d6d2c70d3fe4fc85a
vishalx06 <vishalx06@gmail.com>
2024-03-20 17:26:02 +0530

    Initial Commit

---
+++ swiftfulThinkingBootcamp/swiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulThinkingBootcampApp.swift
+//  swiftfulThinkingBootcamp
+//
+//  Created by Vishal Sharma on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
